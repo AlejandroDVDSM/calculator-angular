d9e50d1d86191ccafb703767ff4f1ff54534eb9e
AlejandroDVDSM <bysalexx@gmail.com>
2022-03-23 12:40:45 +0000

    Calculator done

--- calculator-angular/src/app/app.component.ts
+++ calculator-angular/src/app/app.component.ts
@@ -6,35 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ejercicio con Texto';
-  text1="1";
-  text2="2";
-  num1=0;
-  num2=0;
-  result="X";
+  title = 'Calculator in Angular';
+  operation: string = "";
 
-  concatenate():void{
-    this.result=this.text1+this.text2;
+  buildOperation(value: string): void {
+    switch(value) {
+      case 'Clear':
+        this.operation = "";
+        break;
+      
+      case 'Delete':
+        this.operation = this.operation.slice(0, -1);
+        break;
+
+        default:
+        this.operation += value;
+    }
   }
-  upper():void{
-    this.result=this.text1.toUpperCase();
-  }
-  lower():void{
-    this.result=this.text1.toLowerCase();
-  }
-  find():void{
-    this.result=(this.text1.indexOf(this.text2)).toString();
-  }
-  slice():void{
-    this.result=this.text1.slice(this.num1,this.num2);
-  }
-  length():void{
-    this.result=(this.text1.length).toString();
-  }
-  include():void{
-    this.result=String(this.text1.includes(this.text2));
-  }
-  split():void{
-    this.result=((this.text1).split('this.text2')).toString();
+
+  solve(): void {
+    try {
+      this.operation = eval(this.operation);
+    } catch (exception) {
+      this.operation = "SyntaxError ..."
+      setTimeout(() => {this.operation = ""}, 2000);
+    }
   }
 }
